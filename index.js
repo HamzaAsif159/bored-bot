e38f51a6933e8cc4b153dcc74d3d7fc09781aceb
Hamza Asif <98582615+Hamzi-codes@users.noreply.github.com>
2022-09-01 15:55:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("trigger-btn").addEventListener("click", () => {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("ideas").textContent = `
+            ${data.activity}
+            `
+        })
+})
